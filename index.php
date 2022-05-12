0c67ecab5c6628d2a9278030be90d1242e5c45c6
jade@itnt.co.za <jade@itnt.co.za>
2022-05-12 23:21:07 +0200

    added a western scene to the homepage to go with the theme of the site

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<link rel="stylesheet" href="./assets/css/style.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Courier+Prime&display=swap" rel="stylesheet"> 
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 	<script src="./assets/js/main.js" defer></script>
 	<script src="./assets/js/theme_selection.js" defer></script>
@@ -18,22 +18,22 @@
 		<div class="logo-container">
 			<img src="./assets/img/logo.png" alt="Jade Freel Logo">
 		</div>
-		<div class="theme-selector-container">
-			<a href="#" id="theme-selection"><span class="theme-selector icon"><?php include "./assets/img/svg/themeSelector.svg"; ?></span></a>
-		</div>
+		<ul class="nav-icons-container">
+			<li class="theme-selector"><a href="#" id="theme-selection"><span class="icon"><?php include "./assets/img/svg/themeSelector.svg"; ?></span></a></li>
+			<li class="linkedin"><a href="https://www.linkedin.com/in/jade-freel-a17990210/" target="_blank"><span class="icon"><?php include "./assets/img/svg/linkedin.svg"; ?></span></a></li>
+			<li class="github"><a href="https://github.com/Freel11" target="_blank"><span class="icon"><?php include "./assets/img/svg/github.svg" ?></span></a></li>
+			<li class="codepen"><a href="https://codepen.io/Jade11" target="_blank"><span class="icon"><?php include "./assets/img/svg/codepen.svg" ?></span></a></li>
+		</ul>
 	</header>
 	<section class="hero-section" id="heroSection">
 		<div class="left-side">
+			<h5>Hi there, <span>&#x1F335;</span> Im</h5>
+			<h1 class="name">Jade</h1>
+			<h3>Frontend Developer | UX Designer</h3>
 		</div>
 		<div class="right-side">
-			<div class="pill-slide-container left-pill-container">
-				<div class="pill-slide left-pill"><span>Work</span></div>
-			</div>
-			<div class="pill-slide-container center-pill-container">
-				<div class="pill-slide center-pill"><span>Social</span></div>
-			</div>
-			<div class="pill-slide-container right-pill-container">
-				<div class="pill-slide right-pill"><span>About</span></div>
+			<div class="scene-container">
+				<?php include "./assets/img/svg/scene.svg" ?>
 			</div>
 		</div>
 	</section>
